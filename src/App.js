4e49fd1919bc6fcb9e1d3c920e69ee5d7174efae
SpehtOlena <speht_olena@ukr.net>
2023-10-10 20:15:43 +0300

    Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,135 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { axiosRequest } from "./redux/actions";
+import { Button, Card, Col, Form, Input, Row, Space, Typography, Layout, Menu, theme } from "antd";
+import { Header, Footer, Content } from "antd/es/layout/layout";
+import { useForm } from "antd/es/form/Form";
+import UsersList from "./pages/UsersList/UsersList";
+import AddUser from "./pages/AddUser/AddUser";
 
 function App() {
 
+	const [form] = useForm();
+	const [activePage, setActivePage] = useState();
+	const dispatch = useDispatch();
+	const users = useSelector(state => state.users.data);
+	const [activeUser, setActiveUser] = useState({});
+
+	useEffect(() => {
+		dispatch(axiosRequest('', 'users', ''))
+	}, []);
+
+	useEffect(() => {
+		form.setFieldsValue(activeUser)
+	}, [activeUser]);
+
+	const menuItems = [
+		{
+			key: 'UsersList',
+			label: 'Users',
+			element: <UsersList />
+		},
+		{
+			key: 'AddUser',
+			label: 'Add user',
+			element: <AddUser />
+		}
+	]
+	const onFinish = (values) => {
+		console.log('Success:', values);
+	};
+	const onFinishFailed = (errorInfo) => {
+		console.log('Failed:', errorInfo);
+	};
+	const callback = (value) => {
+		setActiveUser(value)
+	}
+
 	return (
-		<div className="App">
-			App
-		</div>
+		<Layout className="layout" style={{ minHeight: '100vh' }}>
+			<Header
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+				}}
+			>
+				<div className="demo-logo" />
+				<Menu
+					theme="dark"
+					mode="horizontal"
+					items={menuItems}
+					style={{ width: '100%' }}
+					onClick={(item) => {
+						console.log(item);
+					}}
+				/>
+			</Header>
+			<Content
+				style={{
+					padding: '0 50px',
+				}}
+			>
+				<div className="site-layout-content">
+					{activePage}
+				</div>
+			</Content>
+			<Footer
+				style={{
+					textAlign: 'center',
+				}}
+			>
+				Ant Design ©2023 Created by Ant UED
+			</Footer>
+		</Layout>
+
 	);
 }
 
 export default App;
+
+// <Row justify={'space-between'}>
+// 			<Col span={18}>
+// 				<Space wrap>
+// 					{
+// 						users.map(value => (
+// 							<Card onClick={() => callback(value)} key={value.id} style={{ cursor: 'pointer' }}>
+// 								<Typography.Text code>{value.id}</Typography.Text>
+// 								<Typography.Title>{value.username}</Typography.Title>
+// 								<img src={value.img} alt="" />
+// 							</Card>
+// 						))
+// 					}
+// 				</Space>
+// 			</Col>
+// 			<Col span={4}>
+// 				{
+// 					!!activeUser.id &&
+// 					<Form
+// 						name="basic"
+// 						form={form}
+// 						onFinish={onFinish}
+// 						onFinishFailed={onFinishFailed}
+// 						initialValues={activeUser}>
+// 						<Form.Item
+// 							name={'username'}
+// 							label={'username'}>
+// 							<Input />
+// 						</Form.Item>
+// 						<Form.Item
+// 							name={'password'}
+// 							label={'password'}>
+// 							<Input.Password />
+// 						</Form.Item>
+// 						<Form.Item>
+// 							<Button type={'primary'}>Submit</Button>
+// 						</Form.Item>
+// 					</Form>
+
+// 				}
+// 			</Col>
+
+// 		</Row>
+
+
 
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,10 @@
 import axios from "axios"
-import { EDIT_USER, GET_USERS } from "./types/USER"
+import { CREATE_USER, DELETE_USER, EDIT_USER, GET_USER, GET_USERS } from "./types/USER"
 
 const URL = 'http://localhost:3000/'
 
-function axiosRequest(data, namePage, request) {
-	const httpRequest = request.toLowerCase()
+export function axiosRequest(data, namePage, request) {
+	const httpRequest = request.toLowerCase();
 	switch (request) {
 		default:
 			return async dispatch => {
@@ -19,7 +19,34 @@ function axiosRequest(data, namePage, request) {
 				axios
 					.post(URL + namePage, data)
 					.then(response => {
+						dispatch({ type: CREATE_USER, payload: response.data })
+					})
+			}
+		}
+		case 'put': {
+			return async dispatch => {
+				axios
+					.put(`${URL}${namePage}/${data.id}`, data)
+					.then(response => {
 						dispatch({ type: EDIT_USER, payload: response.data })
+					})
+			}
+		}
+		case 'delete': {
+			return async dispatch => {
+				axios
+					.delete(`${URL}${namePage}/${data.id}`)
+					.then(response => {
+						dispatch({ type: DELETE_USER, payload: response.data })
+					})
+			}
+		}
+		case 'get': {
+			return async dispatch => {
+				axios
+					.get(`${URL}${namePage}/${data.id}`)
+					.then(response => {
+						dispatch({ type: GET_USER, payload: response.data })
 					})
 			}
 		}
